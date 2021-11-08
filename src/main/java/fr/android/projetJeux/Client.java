10f306ec16afc6d785da2f6c058248d1124e2bee
maxime <max.poujol21@gmail.com>
2021-11-08 16:07:35 +0100

    Changes by neanb

--- src/main/java/fr/android/projetJeux/Client.java
+++ src/main/java/fr/android/projetJeux/Client.java
@@ -1,24 +1,29 @@
 package fr.android.projetJeux;
 
+import fr.android.projetJeux.utils.SaisieClavier;
 import fr.umontpellier.iut.FX.Software;
 import fr.umontpellier.iut.thread.Listen;
 import fr.umontpellier.iut.thread.Send;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Client extends Application {
+public class Client {
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
 
     public static void main(String[] args) {
         //launch();
         new Client().start();
     }
 
-    public void start(){
+    public void start() {
 
         try {
             Socket client = new Socket("127.0.0.1", 4000);
@@ -28,11 +33,10 @@ public class Client extends Application {
             ExecutorService es = Executors.newFixedThreadPool(10);
             es.execute(new Send(client));
             es.execute(new Listen(client));
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("SERVER NOT FOUND\nPlease start the server, then restart the application");
             //Software.getMessages().setText();
         }
-
 
 
     }
@@ -45,18 +49,29 @@ public class Client extends Application {
     }*/
 
 
-    private boolean setIdentifiant(){
+    private boolean setIdentifiant(Socket socket) {
+
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+            String response;
+            do {
+                System.out.println("Entrez un pseudo: ");
+                out.writeObject(SaisieClavier.saisieTerminal());
+                response = (String) in.readObject();
+                System.out.println(response);
+            } while (!response.equals("OK"));
 
 
-
-
-
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
         return false;
 
 
     }
-
 
 
 }
